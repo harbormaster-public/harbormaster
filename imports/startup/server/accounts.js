6a66f91416600e3cee6ae0dc000fea020f4f75cd
Skyler Brungardt <skyler.brungardt@gmail.com>
2017-06-28 11:53:36 -0700

    Sending better password reset emails
    Now they're customized to be more sensibly from Harbormaster, rather
    than just stock Meteor.

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,13 +1,27 @@
 import { Users } from '../../api/users';
 
+const SITE_NAME = 'Harbormaster';
+const FROM = 'Harbormaster <noreply@harbormaster>';
+
 Accounts.onLogin(function () {
-  var user_id = Meteor.user().emails[0].address;
-  var harbormaster = Users.find().fetch().length ? false : true;
-  harbormaster = Users.findOne(user_id) ?
-    Users.findOne(user_id).harbormaster :
-    harbormaster
-  ;
-  Users.upsert(user_id, {
-    harbormaster: harbormaster
-  });
+  let user_id = Meteor.user().emails[0].address;
+  let harbormaster = Users.find().fetch().length ? false : true;
+  let user = Users.findOne(user_id);
+
+  harbormaster = user ?  user.harbormaster : harbormaster;
+
+  Users.upsert(user._id, { harbormaster: harbormaster });
+
+  //TODO: plugins
 });
+
+Accounts.emailTemplates.siteName = SITE_NAME;
+Accounts.emailTemplates.from = FROM;
+
+Accounts.emailTemplates.resetPassword = {
+  subject () { return 'Set Your Harbormaster Account Password' },
+  text (user, url) {
+    let email = user.emails[0].address;
+    return `Click this link to set the password for ${email}: ${url}`
+  }
+};
