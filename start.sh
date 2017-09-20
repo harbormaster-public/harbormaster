3d2711a0aefd705c0c8c43e71cb981fbbf60f9f3
Skyler Brungardt <skyler.brungardt@gmail.com>
2017-09-19 17:07:59 -0700

    FQDN fix for faster sendmail bootup

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+ip_address=$(awk 'END{print $1}' /etc/hosts)
+
+echo "$ip_address localhost localhost.localdomain harbormaster" >> /etc/hosts
+
 service sendmail start
 node /harbormaster/bundle/main.js
