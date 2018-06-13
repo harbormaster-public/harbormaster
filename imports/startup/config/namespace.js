135db0d287e6b9f1d055cae7aafa9d1610c51f1e
Skyler Brungardt <skyler.brungardt@gmail.com>
2018-06-12 18:00:29 -0700

    Updating Meteor, adding more namespace

--- imports/startup/config/namespace.js
+++ imports/startup/config/namespace.js
@@ -17,3 +17,5 @@ $H.start_shipment = function (lane_id, manifest, date) {
 $H.end_shipment = function (lane, exit_code, manifest) {
   return Meteor.call('Lanes#end_shipment', lane, exit_code, manifest);
 };
+
+$H.bind = $H.bindEnvironment = Meteor.bindEnvironment;
