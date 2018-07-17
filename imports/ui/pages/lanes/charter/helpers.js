114fa450e018a1bcc29a0c179e0802f5ef2f4d5d
Skyler Brungardt <skyler.brungardt@gmail.com>
2018-07-16 18:11:12 -0700

    Taking apart charter rendering

--- imports/ui/pages/lanes/charter/helpers.js
+++ imports/ui/pages/lanes/charter/helpers.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 import { Lanes } from '../../../../api/lanes';
 import { Shipments } from '../../../../api/shipments';
+import { get_lane } from '../lib/util';
 
 import * as d3 from 'd3';
 
@@ -10,21 +12,51 @@ const SALVAGE = 'SALVAGE';
 const TOP_PADDING = 185;
 const FOLLOWUP_COLOR = '#0af';
 const SALVAGE_COLOR = '#fa0';
+const root_lane = new ReactiveVar({});
+const width = new ReactiveVar(0);
+const height = new ReactiveVar(0);
 
 Template.charter.onCreated(function () {
   let options = {
     sort: { actual: -1 },
-    limit: 1,
+    limit: 1
   };
 
-  Lanes.find().forEach((lane) => {
-    Meteor.subscribe('Shipments', lane, options);
+  this.autorun(() => {
+    Meteor.subscribe('Lanes');
+    Lanes.find().forEach((lane) => {
+      Meteor.subscribe('Shipments', lane, options);
+    });
   });
 });
 
 Template.charter.onRendered(function () {
+
+  let draw_height = $('body').height();
+  $('#graph').height(draw_height - TOP_PADDING);
+
+  width.set($('#graph').outerWidth());
+  height.set($('#graph').outerHeight());
+
+  let treemap = d3.tree().size([width.get(), height.get()]);
+
+  let svg = d3.select('#graph').append('svg')
+    .attr('width', width.get())
+    .attr('height', height.get());
+
+  let g = svg.append('g')
+    .attr('transform', 'translate(0, 50)');
+
+  window.onresize = () => {
+    width.set($('#graph').outerWidth());
+    height.set($('#graph').outerHeight());
+  };
+
   this.autorun(() => {
-    let shipments = Shipments.find().fetch(); // Triggers autorun
+    let nodes = treemap(d3.hierarchy(root_lane.get()));
+
+    treemap.size([width.get(), height.get()]);
+    svg.attr('width', width.get()).attr('height', height.get());
 
     d3.selectAll('circle')
       .attr('fill', (d) => {
@@ -57,143 +89,131 @@ Template.charter.onRendered(function () {
           latest_shipment.start :
           ''
         ;
-        let url = `/lanes/${d.data.name}/ship/${start}`;
+        let url = `/lanes/${d.data.slug}/ship/${start}`;
         return url;
       })
     ;
+
+    g.selectAll('.node')
+      .data(nodes.descendants())
+      .enter().append('path')
+      .attr('d', (d) => {
+        if (! d.parent) return '';
+        return `M${d.x},${d.y / 2}
+          C${d.x},${(d.y + d.parent.y) / 4}
+           ${d.parent.x},${(d.y + d.parent.y) / 4}
+           ${d.parent.x},${d.parent.y}
+        `;
+      })
+      .attr('fill', 'none')
+      .attr('stroke', (d) => {
+        let stroke_color;
+        switch (d.data.role) {
+          case 'FOLLOWUP':
+            stroke_color = FOLLOWUP_COLOR;
+            break;
+          case 'SALVAGE':
+            stroke_color = SALVAGE_COLOR;
+            break;
+          default:
+            stroke_color = '#f09';
+            break;
+        }
+
+        return stroke_color;
+      })
+    ;
+    let node = g.selectAll('.node')
+      .data(nodes.descendants())
+      .enter()
+    ;
+    let group = node.append('g')
+      .attr("class", (d) => {
+        return "node" + (d.children ? " node-internal" : " node-leaf");
+      })
+      .attr("transform", (d) => {
+        return "translate(" + d.x + "," + d.y / 2 + ")";
+      })
+    ;
+    let a = group.append('a')
+      .attr('class', 'charter-link')
+    ;
+
+    a.append('circle')
+      .attr('r', 15)
+      .attr('stroke', (d) => {
+        let stroke_color;
+        switch (d.data.role) {
+          case 'FOLLOWUP':
+            stroke_color = FOLLOWUP_COLOR;
+            break;
+          case 'SALVAGE':
+            stroke_color = SALVAGE_COLOR;
+            break;
+          default:
+            stroke_color = '#f09';
+            break;
+        }
+
+        return stroke_color;
+      })
+      .attr('fill', (d) => {
+        let latest_shipment = Shipments.findOne(
+          { lane: d.data._id },
+          { $sort: { finished: -1 }, limit: 1 }
+        );
+        let fill_color;
+
+        if (! latest_shipment) return fill_color = 'transparent';
+
+        if (latest_shipment.exit_code == 0) {
+          fill_color = 'rgba(0, 255, 0, 0.25)';
+        }
+
+        if (latest_shipment.exit_code > 0) {
+          fill_color = 'rgba(255, 0, 0, 0.25)';
+        }
+
+        return fill_color;
+      })
+    ;
+    a.append('text')
+      .attr('y', (d) => { return d.children ? -20 : 40; })
+      .style('text-anchor', 'middle')
+      .text((d) => { return d.data.name; })
+      .attr('stroke', 'none')
+      .attr('fill', (d) => {
+        let fill_color;
+        switch (d.data.role) {
+          case 'FOLLOWUP':
+            fill_color = FOLLOWUP_COLOR;
+            break;
+          case 'SALVAGE':
+            fill_color = SALVAGE_COLOR;
+            break;
+          default:
+            fill_color = '#f09';
+            break;
+        }
+
+        return fill_color;
+      })
+    ;
   });
-
-  let draw_height = $('body').height();
-  $('#graph').height(draw_height - TOP_PADDING);
-
-  let lane = Session.get('charter_tree') || {};
-
-  let width = $('#graph').outerWidth();
-  let height = $('#graph').outerHeight();
-
-  let treemap = d3.tree().size([width, height]);
-  let nodes = treemap(d3.hierarchy(lane));
-
-  let svg = d3.select('#graph').append('svg')
-    .attr('width', width)
-    .attr('height', height);
-  let g = svg.append('g')
-    .attr('transform', 'translate(0, 50)');
-  let link = g.selectAll('.link')
-    .data(nodes.descendants().slice(1))
-    .enter().append('path')
-    .attr('d', (d) => {
-      return "M" + d.x + "," + d.y / 4
-        + "C" + d.x + "," + (d.y + d.parent.y) / 8
-        + " " + d.parent.x + "," + (d.y + d.parent.y) / 8
-        + " " + d.parent.x + "," + d.parent.y / 4;
-    })
-    .attr('fill', 'none')
-    .attr('stroke', (d) => {
-      let stroke_color;
-      switch (d.data.role) {
-        case 'FOLLOWUP':
-          stroke_color = FOLLOWUP_COLOR;
-          break;
-        case 'SALVAGE':
-          stroke_color = SALVAGE_COLOR;
-          break;
-        default:
-          stroke_color = '#f09';
-          break;
-      }
-
-      return stroke_color;
-    })
-  ;
-  let node = g.selectAll('.node')
-    .data(nodes.descendants())
-    .enter().append('g')
-    .attr("class", (d) => {
-      return "node" + (d.children ? " node-internal" : " node-leaf");
-    })
-    .attr("transform", (d) => {
-      return "translate(" + d.x + "," + d.y / 4 + ")";
-    })
-    .append('a')
-    .attr('class', 'charter-link')
-  ;
-
-  node.append('circle')
-    .attr('r', 15)
-    .attr('stroke', (d) => {
-      let stroke_color;
-      switch (d.data.role) {
-        case 'FOLLOWUP':
-          stroke_color = FOLLOWUP_COLOR;
-          break;
-        case 'SALVAGE':
-          stroke_color = SALVAGE_COLOR;
-          break;
-        default:
-          stroke_color = '#f09';
-          break;
-      }
-
-      return stroke_color;
-    })
-    .attr('fill', (d) => {
-      let latest_shipment = Shipments.findOne(
-        { lane: d.data._id },
-        { $sort: { finished: -1 }, limit: 1 }
-      );
-      let fill_color;
-
-      if (! latest_shipment) return fill_color = 'transparent';
-
-      if (latest_shipment.exit_code == 0) {
-        fill_color = 'rgba(0, 255, 0, 0.25)';
-      }
-
-      if (latest_shipment.exit_code > 0) {
-        fill_color = 'rgba(255, 0, 0, 0.25)';
-      }
-
-      return fill_color;
-    })
-  ;
-  node.append('text')
-    .attr('y', (d) => { return d.children ? -20 : 40; })
-    .style('text-anchor', 'middle')
-    .text((d) => { return d.data.name; })
-    .attr('stroke', 'none')
-    .attr('fill', (d) => {
-      let fill_color;
-      switch (d.data.role) {
-        case 'FOLLOWUP':
-          fill_color = FOLLOWUP_COLOR;
-          break;
-        case 'SALVAGE':
-          fill_color = SALVAGE_COLOR;
-          break;
-        default:
-          fill_color = '#f09';
-          break;
-      }
-
-      return fill_color;
-    })
-  ;
-
 });
 
 Template.charter.helpers({
 
   lane () {
     let name = FlowRouter.getParam('name');
-    let lane = Lanes.findOne({ name: name });
+    let lane = get_lane(name);
 
     return lane;
   },
 
   build_graph () {
     let name = FlowRouter.getParam('name');
-    let lane = Lanes.findOne({ name: name });
+    let lane = get_lane(name);
     if (! lane) return false;
 
     let assign_children = (target) => {
@@ -224,9 +244,9 @@ Template.charter.helpers({
 
     lane = assign_children(lane);
 
-    Session.set('charter_tree', lane);
+    root_lane.set(lane);
 
-    return;
-  },
+    return '';
+  }
 
 });
