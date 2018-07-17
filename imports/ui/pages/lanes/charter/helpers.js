cd36e4ea6aaaa9c318e5905dcf38529cbcff3ead
Skyler Brungardt <skyler.brungardt@gmail.com>
2018-07-17 00:19:33 -0700

    Using dagre layout, with links

--- imports/ui/pages/lanes/charter/helpers.js
+++ imports/ui/pages/lanes/charter/helpers.js
@@ -5,6 +5,9 @@ import { Shipments } from '../../../../api/shipments';
 import { get_lane } from '../lib/util';
 
 import cytoscape from 'cytoscape';
+import dagre from 'cytoscape-dagre';
+
+cytoscape.use(dagre);
 
 const ROOT = 'ROOT';
 const FOLLOWUP = 'FOLLOWUP';
@@ -13,7 +16,8 @@ const TOP_PADDING = 185;
 const FOLLOWUP_COLOR = '#0af';
 const SALVAGE_COLOR = '#fa0';
 const ROOT_COLOR = '#f0a';
-const root_lane = new ReactiveVar({});
+const SUCCESS_COLOR = '#3adb76';
+const FAIL_COLOR = 'red';
 const node_list = new ReactiveVar([]);
 
 Template.charter.onCreated(function () {
@@ -43,6 +47,8 @@ Template.charter.onRendered(function () {
         style: {
           'content': 'data(name)',
           'background-color': 'data(color)',
+          'border-width': '3px',
+          'border-color': 'data(border)',
         },
       },
       {
@@ -57,13 +63,26 @@ Template.charter.onRendered(function () {
     ],
   });
 
+  $('#graph').on('mouseout', () => document.body.style.cursor = '');
+  cy.on('mouseout', 'node', () => document.body.style.cursor = '');
+  cy.on('mouseover', 'node', () => document.body.style.cursor = 'pointer');
+  cy.on('tap', 'node', (e) => {
+    let data = e.target.data();
+    let start = data.shipment ? data.shipment.start : '';
+    let url = `/lanes/${data.lane.slug}/ship/${start}`;
+    document.body.style.cursor = '';
+    FlowRouter.go(url);
+  });
+
   this.autorun(() => {
     let graph = node_list.get();
+    cy.remove('node');
     cy.add(graph);
     cy.layout({
-      name: 'circle',
+      name: 'dagre',
       fit: true,
-      avoidOverlap: true,
+      rankDir: 'LR',
+      nodeDimensionsIncludeLabels: true,
     }).run();
   });
 });
@@ -87,6 +106,14 @@ Template.charter.helpers({
       let followup = Lanes.findOne(target.followup);
       let plan = Lanes.findOne(target.salvage_plan);
       if (followup && ! target.recursive) {
+        let last_shipment = Shipments.findOne({ lane: followup._id });
+        let color = FOLLOWUP_COLOR;
+
+        if (last_shipment && last_shipment.exit_code) color = FAIL_COLOR;
+        else if (
+          last_shipment && last_shipment.exit_code == 0
+        ) color = SUCCESS_COLOR;
+
         followup.role = FOLLOWUP;
         followup.parent = target._id;
         followup.recursive = followup._id == target._id ? true : false;
@@ -96,7 +123,10 @@ Template.charter.helpers({
           data: {
             id: followup._id,
             name: followup.name,
-            color: FOLLOWUP_COLOR,
+            color,
+            border: FOLLOWUP_COLOR,
+            lane: followup,
+            shipment: Shipments.findOne({ lane: followup._id }),
           },
         }, {
           group: 'edges',
@@ -109,6 +139,14 @@ Template.charter.helpers({
       }
 
       if (plan && ! target.recursive) {
+        let last_shipment = Shipments.findOne({ lane: followup._id });
+        let color = SALVAGE_COLOR;
+
+        if (
+          last_shipment && last_shipment.exit_code == 0
+        ) color = SUCCESS_COLOR;
+        else if (last_shipment && last_shipment.exit_code) color = FAIL_COLOR;
+
         plan.role = SALVAGE;
         plan.parent = target._id;
         plan.recursive = plan._id == target._id ? true : false;
@@ -118,11 +156,14 @@ Template.charter.helpers({
           data: {
             id: plan._id,
             name: plan.name,
-            color: SALVAGE_COLOR,
+            color,
+            border: SALVAGE_COLOR,
+            lane: plan,
+            shipment: Shipments.findOne({ lane: plan._id }),
           },
         }, {
+          group: 'edges',
           data: {
-            group: 'edges',
             source: target._id,
             target: plan._id,
             color: SALVAGE_COLOR,
@@ -140,19 +181,26 @@ Template.charter.helpers({
 
     lane.children = [];
     lane.role = ROOT;
+    let last_shipment = Shipments.findOne({ lane: lane._id });
+    let color;
+    if (! last_shipment) color = ROOT_COLOR;
+    else if (last_shipment.exit_code) color = FAIL_COLOR;
+    else if (last_shipment.exit_code == 0) color = SUCCESS_COLOR;
 
     list.push({
       group: 'nodes',
       data: {
         id: lane._id,
         name: lane.name,
-        color: ROOT_COLOR,
+        color,
+        border: ROOT_COLOR,
+        lane: lane,
+        shipment: last_shipment,
       },
     });
     lane = assign_children(lane);
 
     node_list.set(list);
-    root_lane.set(lane);
 
     return '';
   },
