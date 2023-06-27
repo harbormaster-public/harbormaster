6c6495fc93bb7c747dded48800bf51744bdd05ad
Skyler Brungardt <skyler.brungardt@gmail.com>
2023-06-27 12:27:18 -0700

    Remainder of UI unit tests, Root & User pages

--- imports/ui/pages/root/root.test.js
+++ imports/ui/pages/root/root.test.js
@@ -1,40 +1,79 @@
-// import {
-  // shipments_last_24_hours,
-  // latest_shipment,
-  // total_captains,
-  // total_harbormasters,
-  // total_shipments,
-  // is_harbormaster,
-  // is_captain,
-// } from './lib';
+import { expect } from 'chai';
+import {
+  shipments_last_24_hours,
+  latest_shipment,
+  total_captains,
+  total_harbormasters,
+  is_harbormaster,
+  is_captain,
+} from './lib';
+import { resetDatabase } from 'cleaner';
+const call_method = H.call;
 
 describe('Root Page (/)', () => {
 
   describe('#shipments_last_24_hours', function () {
-    it('returns the total_shipments in locale string');
+    it('returns the total_shipments in locale string', () => {
+      expect(shipments_last_24_hours()).to.eq('Loading');
+    });
   });
 
   describe('#latest_shipment', function () {
-    it('returns a loading object if no shipment is available');
-    it('gets the latest shipment date and saves it in Session');
-    it('returns the latest shipment date from Session');
+    it('returns a loading object if no shipment is available', () => {
+      expect(latest_shipment().locale).to.eq('loading...');
+    });
+    it('gets the latest shipment date and saves it in Session', () => {
+      H.call = (method) => expect(method).to.eq('Shipments#get_latest_date');
+      latest_shipment();
+      H.call = call_method;
+    });
+    it('returns the latest shipment date from Session', () => {
+      H.call = () => { };
+      H.Session.set('latest_shipment', 'test');
+      expect(latest_shipment()).to.eq('test');
+      H.call = call_method;
+    });
   });
 
   describe('#total_captains', function () {
-    it('returns the total number of captains across all lanes');
+    it('returns the total number of captains across all lanes', () => {
+      resetDatabase(null);
+      expect(total_captains()).to.eq('0');
+      Factory.create('lane', { captains: ['test@harbormaster.io'] });
+      expect(total_captains()).to.eq('1');
+    });
   });
 
   describe('#total_harbormasters', function () {
-    it('returns the total number of current harbormasters');
+    it('returns the total number of current harbormasters', () => {
+      resetDatabase(null);
+      expect(total_harbormasters()).to.eq('0');
+      Factory.create('user', { harbormaster: true });
+      expect(total_harbormasters()).to.eq('1');
+    });
   });
 
   describe('#is_harbormaster', function () {
-    it('returns true if current the user is a harbormaster');
+    it('returns true if current the user is a harbormaster', () => {
+      resetDatabase(null);
+      Factory.create('user', {
+        _id: H.user().emails[0].address,
+        harbormaster: true,
+      });
+      expect(is_harbormaster()).to.eq(true);
+    });
   });
 
   describe('#is_captain', function () {
-    it('returns true if the user is captain of any lanes');
-    it('returns false otherwise');
+    it('returns true if the user is captain of any lanes', () => {
+      resetDatabase(null);
+      Factory.create('lane', { captains: [H.user().emails[0].address] });
+      expect(is_captain()).to.eq(true);
+    });
+    it('returns false otherwise', () => {
+      resetDatabase(null);
+      expect(is_captain()).to.eq(false);
+    });
   });
 
 });
--- imports/ui/pages/users/lib.js
+++ imports/ui/pages/users/lib.js
@@ -9,23 +9,25 @@ const is_harbormaster = function (user) {
 };
 
 const captain_lanes = function (user) {
-  var pliable_lanes = Lanes.find({ $or: [
-    {captains: { $in: [user._id] } },
-    { tokens: { $exists: true }},
-  ]}).fetch();
+  var pliable_lanes = Lanes.find({
+    $or: [
+      { captains: { $in: [user._id] } },
+      { tokens: { $exists: true } },
+    ]
+  }).fetch();
   var lane_names = [];
 
   if (user.harbormaster) { return 'All'; }
   _.each(pliable_lanes, function (lane) {
     if (
-        // user webhook assigned
-        (lane.tokens && Object.values(lane.tokens).includes(user._id)) ||
-        // user is assigned as captain
-        !lane.tokens
-      ) {
-        lane_names.push(lane.name);
-      }
-    });
+      // user webhook assigned
+      (lane.tokens && Object.values(lane.tokens).includes(user._id)) ||
+      // user is assigned as captain
+      !lane.tokens
+    ) {
+      lane_names.push(lane.name);
+    }
+  });
 
   return lane_names.length ? lane_names.join(', ') : 'None';
 };
@@ -33,12 +35,12 @@ const captain_lanes = function (user) {
 const expire_user = function (user) {
   const confirm_message = `Expire user?\n${user._id}`;
 
-  if (window.confirm(confirm_message)) {
+  if (H.confirm(confirm_message)) {
     H.call('Users#expire_user', user._id, (err, res) => {
       if (err) throw err;
 
       console.log('User expired:', res);
-      alert(`User expired: ${res}`);
+      H.alert(`User expired: ${res}`);
     });
   }
 };
