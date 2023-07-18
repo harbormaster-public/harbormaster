5f5f1fd4072f43b0ecfe6c5dfe532de1ad06ff8c
Skyler Brungardt <skyler.brungardt@gmail.com>
2023-07-18 15:20:36 -0700

    Accounts methods unit tests

--- imports/startup/server/accounts.test.js
+++ imports/startup/server/accounts.test.js
@@ -1,8 +1,24 @@
-import {set_harbormaster} from './accounts';
+import { resetDatabase } from 'cleaner';
+import { set_harbormaster } from './accounts';
+import { expect } from 'chai';
+import { Users } from '../../api/users';
 
 describe('Accounts.onLogin', () => {
   describe('#set_harbormaster', () => {
-    it('assigns a new Harbormaster if there are no users');
-    it('assigns existing Harbormaster status for existing users');
+    beforeEach(() => {
+      resetDatabase(null);
+    });
+    it('assigns a new Harbormaster if there are no users', () => {
+      set_harbormaster('test@harbormaster.io');
+      expect(Users.findOne('test@harbormaster.io').harbormaster).to.eq(true);
+    });
+    it('assigns existing Harbormaster status for existing users', () => {
+      Factory.create('user', {
+        _id: 'test@harbormaster.io',
+        harbormaster: false,
+      });
+      set_harbormaster('test@harbormaster.io');
+      expect(Users.findOne('test@harbormaster.io').harbormaster).to.eq(false);
+    });
   });
 });
