80345ad79ee69ed79bd05b7a1596818b4712bdce
Skyler Brungardt <skyler.brungardt@gmail.com>
2023-09-12 12:30:18 -0700

    Updating README

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,4 +2,7 @@
 
 echo "`date`: Running pre-commit hook."
 npm run test
-echo "`date`: Test complete.  Continuing with commit."
+echo "`date`: Test complete.  Updating README with status."
+./update_build_status.sh
+git add README.md
+git commit --amend -C HEAD --no-verify
